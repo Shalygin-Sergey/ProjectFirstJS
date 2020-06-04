bb3d9e97dbda60f267a334cbe3af4fdeb0683adc
Shalygin-Sergey <60192169+Shalygin-Sergey@users.noreply.github.com>
2020-06-04 22:54:34 +0300

    Создан новый файл css и js для 1 домашнего задания

---
+++ js/firstHomeWork.js
@@ -0,0 +1,9 @@
+alert('Hello, dear man!');
+console.log('Если ты видишь это, значит ты в консоли!');
+
+const money = '';
+const income = '';
+const addExpenses = '';
+const deposit = '';
+const mission = '';
+const period = '';
--- js/script.js
+++
@@ -1,9 +0,0 @@
-alert('Hello, dear man!');
-console.log('Если ты видишь это, значит ты в консоли!');
-
-const money = '';
-const income = '';
-const addExpenses = '';
-const deposit = '';
-const mission = '';
-const period = '';
