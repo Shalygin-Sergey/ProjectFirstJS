d3d2b0fa531a66fef66eba6d2f0d203c44400004
Shalygin-Sergey <60192169+Shalygin-Sergey@users.noreply.github.com>
2020-06-08 19:28:09 +0300

    Вычислили бюджет на месяц, обязательные расходы, бюджет на день

--- js/firstHomeWork.js
+++ js/firstHomeWork.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = 30000;
 let income = 'фриланс';
 let addExpenses = 'Интернет, Комуналка, Еда, Развлечения, Ребенок, Учеба';
@@ -18,4 +20,34 @@ console.log('Цель заработать ' + mission + ' рублей');
 addExpenses = addExpenses.toLowerCase().split(' ');
 console.log(addExpenses);
 
-console.log(budgetDay);
+// console.log(budgetDay);
+
+money = prompt('Ваш месячный доход?', '30000');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?', 'интернет');
+let expenses2 = prompt('Введите обязательную статью расходов?', 'продукты');
+
+let amount1 = +prompt('Во сколько это обойдется?', '500');
+let amount2 = +prompt('Во сколько это обойдется?', '1000');
+
+let budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц ' + budgetMonth);
+
+let missionComplite = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута за: ' + missionComplite + ' месяцев');
+
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ' + budgetDay);
+
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay <= 0) {
+    console.log('Что то пошло не так');
+}
