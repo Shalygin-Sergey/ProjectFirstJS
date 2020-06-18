24847eb35b6726f4316924a93081e3bdb6216709
Shalygin-Sergey <60192169+Shalygin-Sergey@users.noreply.github.com>
2020-06-18 23:40:54 +0300

    Создал вызов элементов в консоль

---
+++ js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let calculate = document.querySelector('#start');
+console.log(calculate);
+
+let plusOne = document.getElementsByTagName('button')[0];
+let plusTwo = document.getElementsByTagName('button')[1];
+
+console.log(plusOne);
+console.log(plusTwo);
+
+let checkBox = document.querySelector('#deposit-check');
+console.log(checkBox);
+
+let moneyInput = document.querySelectorAll('.additional_income-item')[0];
+let moneyInput2 = document.querySelectorAll('.additional_income-item')[1];
+console.log(moneyInput);
+console.log(moneyInput2);
+
+let budgetMonthInput = document.querySelector('.budget_month-value');
+console.log(budgetMonthInput);
+
+
+let budgetDayInput = document.querySelector('.budget_day-value');
+console.log(budgetDayInput);
+
+let expensesMonthInput = document.querySelector('.expenses_month-value');
+console.log(expensesMonthInput);
+
+let additionalInput = document.querySelector('.additional_income-value');
+console.log(additionalInput);
+
+let periodInput = document.querySelector('.income_period-value');
+console.log(periodInput);
+
+let targetMonthInput = document.querySelector('.target_month-value');
+console.log(targetMonthInput);
+
+let monthAmount = document.querySelector('.salary-amount');
+console.log(monthAmount);
+let incomeTitle = document.querySelector('.income-title');
+console.log(incomeTitle);
+let incomeAmount = document.querySelector('.income-amount');
+console.log(incomeAmount);
+let expensesTitle = document.querySelector('.expenses-title');
+console.log(expensesTitle);
+let expensesAmount = document.querySelector('.expenses-amount');
+console.log(expensesAmount);
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+console.log(additionalExpensesItem);
+let depositAmount = document.querySelector('.deposit-amount');
+console.log(depositAmount);
+let depositPercent = document.querySelector('.deposit-percent');
+console.log(depositPercent);
+let targetAmount = document.querySelector('.target-amount');
+console.log(targetAmount);
+let periodSelect = document.querySelector('.period-select');
+console.log(periodSelect);
--- js/script.js
+++ js/script.js
@@ -1,41 +1,7 @@
 'use strict';
-// let str = '123выфвыфвы';
 
-// let patternAbc = /[а-яё]\D/gi;
+const mainHead = document.querySelector('.one');
 
-// let result = patternAbc.test(str);
-// console.log(result);
+const computedStyleMainHead = getComputedStyle(mainHead);
 
-// let test = 'какой то текст';
-// [...test.matchAll(patternAbc)]
-
-// const isText = function (str) {
-//     const pattern = /^[а-яё]\D/gi;
-//     return str.match(pattern);
-// }
-
-
-// let arr = ['kokko', 'mokko', 'sagitarius', 'petrovich', 'eduvanchik verhovsjoi'];
-// for (let i = 0; i < arr.length; i++) {
-//     arr[i].trim()[0].toUpperCase() + arr[i].trim().slice(1).toLowerCase();
-// }
-
-// let addEx = [];
-// for (let i = 0; i < arr.length; i++) {
-//     addEx.push(arr[i].trim()[0].toUpperCase() + arr[i].trim().slice(1).toLowerCase());
-// }
-
-// console.log(addEx.join(', '));
-
-
-let arr = appData.addExpenses.split(',');
-for (let i = 0; i < arr.length; i++) {
-    arr[i].trim()[0].toUpperCase() + arr[i].trim().slice(1).toLowerCase();
-}
-
-let addExpenses = [];
-for (let i = 0; i < arr.length; i++) {
-    addExpenses.push(arr[i].trim()[0].toUpperCase() + arr[i].trim().slice(1).toLowerCase());
-}
-
-console.log(addEx.join(', '));
+console.log(computedStyleMainHead);
